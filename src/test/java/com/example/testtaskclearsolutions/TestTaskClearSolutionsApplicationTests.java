c743ce2e533cb681a4e3034ae61414909134e53a
[RZfdMcEh] <[sonyasab2000@gmail.com]>
2024-05-02 22:49:25 +0300

    init commit

---
+++ src/test/java/com/example/testtaskclearsolutions/TestTaskClearSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtaskclearsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskClearSolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
